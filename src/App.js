2b6564cdc0c4b181181d5a0d9682f55bf0e4bf8f
Rita-cedcoss <ritapal@cedcoss.com>
2022-12-09 11:14:26 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
-import NumberGenerator from './RendomNumberGenrator/NumberGenerator';
+import "./App.css";
+import NumberGenerator from "./RendomNumberGenrator/NumberGenerator";
 
-import { ErrorBoundary } from 'react-error-boundary';
-import ErrorBoundaries from './RendomNumberGenrator/ErrorBoundaries';
-import RandomNumberGenerator from './FumctionalCompNumGenerator/RandomNumberGenerator';
-import FunErrorBoundary from './FumctionalCompNumGenerator/FunErrorBoundary';
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorBoundaries from "./RendomNumberGenrator/ErrorBoundaries";
+import RandomNumberGenerator from "./FumctionalCompNumGenerator/RandomNumberGenerator";
+import FunErrorBoundary from "./FumctionalCompNumGenerator/FunErrorBoundary";
 function App() {
   return (
     <div className="App">
       <h1>Task1: Error Boundary using Class Component </h1>
       <ErrorBoundaries>
-      <NumberGenerator/>
+        <NumberGenerator />
       </ErrorBoundaries>
+      <hr/>
       <h1>Task2: Error Boundary using Functional Component</h1>
       <ErrorBoundary FallbackComponent={FunErrorBoundary}>
-         <RandomNumberGenerator/>
+        <RandomNumberGenerator />
       </ErrorBoundary>
     </div>
   );
